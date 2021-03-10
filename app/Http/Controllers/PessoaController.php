55dd7bdfd42b4456de5a24fd9df66b68aac57dd3
Everton Correia <56456963+evtcorreia@users.noreply.github.com>
2021-03-10 11:02:16 -0300

    inclusão de duas rotinas de chamada na API para buscar telefones e enderecos

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -14,17 +14,9 @@ use GuzzleHttp\Client;
             $response = $client->get('http://api.hml01.com.br/api/pessoa/72990385671');
             $pessoas = json_decode($response->getBody(), true);
 
-
-            $client = new Client();
-            $response = $client->get('http://api.hml01.com.br/api/paciente/72990385671');
-            $paciente = json_decode($response->getBody(),true);
-
-            //dd($paciente);
-
-
         return view('/paciente/index/index',[
                 'pessoa' => $pessoas,
-                'paciente' => $paciente,
+                
                 
             ]);
         }
@@ -40,10 +32,20 @@ use GuzzleHttp\Client;
             $response = $client->get('http://api.hml01.com.br/api/paciente/72990385671');
             $paciente = json_decode($response->getBody(),true);
 
+            $client = new Client();
+            $response  = $client->get('http://api.hml01.com.br/api/telefone/72990385671');
+            $telefones = json_decode($response->getBody(), true);
+
+            $client = new Client();
+            $response = $client->get('http://api.hml01.com.br/api/endereco/72990385671');
+            $endereco = json_decode($response->getBody(),true);
+
 
             return view('/paciente/informacoes/index',[
                 'pessoa' => $pessoas,
                 'paciente' => $paciente,
+                'telefones' => $telefones,
+                'endereco' => $endereco
             ]);
         }
     }
