ebb1b1b20fd8dd2252358b6e58ff0b40d3cf89b0
Everton Correia <56456963+evtcorreia@users.noreply.github.com>
2021-03-09 22:02:20 -0300

    Atualizacao de dados paciente
    Inserida a  rotina para busca dos dados da pessoa como paciente

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -15,9 +15,16 @@ use GuzzleHttp\Client;
             $pessoas = json_decode($response->getBody(), true);
 
 
+            $client = new Client();
+            $response = $client->get('http://api.hml01.com.br/api/paciente/72990385671');
+            $paciente = json_decode($response->getBody(),true);
+
+            //dd($paciente);
+
 
         return view('/paciente/index/index',[
                 'pessoa' => $pessoas,
+                'paciente' => $paciente,
                 
             ]);
         }
@@ -29,9 +36,14 @@ use GuzzleHttp\Client;
             $response = $client->get('http://api.hml01.com.br/api/pessoa/72990385671');
             $pessoas = json_decode($response->getBody(),true);
 
+            $client = new Client();
+            $response = $client->get('http://api.hml01.com.br/api/paciente/72990385671');
+            $paciente = json_decode($response->getBody(),true);
+
 
             return view('/paciente/informacoes/index',[
                 'pessoa' => $pessoas,
+                'paciente' => $paciente,
             ]);
         }
     }
