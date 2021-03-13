e3828e0669f1627bb0c383ed1d48c0b0a8404525
Everton Correia <56456963+evtcorreia@users.noreply.github.com>
2021-03-13 11:46:50 -0300

    Atualização tela consultas

--- resources/views/paciente/consultas/index.blade.php
+++ resources/views/paciente/consultas/index.blade.php
@@ -6,8 +6,8 @@ Consultas
 
 @section('conteudo')
 
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item col-sm-12 col-md-2 col-lg-2  mt-3">
+<ul class="nav nav-tabs" id="myTab" role="tablist" >
+    <li class="nav-item col-sm-12 col-md-2 col-lg-2  mt-3" >
         <a class="nav-link active " id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Em Aberto</a>
     </li>
     <li class="nav-item col-sm-12 col-md-2 col-lg-2 mt-3">
@@ -20,26 +20,43 @@ Consultas
         <div style="background-color: white;">
             <h3 class="ml-2 pt-3">Nome do Paciente</h3>
             <div class="row ml-4">
-                <div class="col col-sm-12 col-md-6 col-lg-6 mb-3 mt-3">
-                    <ul type="circle">
-                        <li>Consulta 12/12/12</li>
-                        <li>consulta x</li>
-                        <li>consulta 23</li>
+                <div class="col col-sm-12 col-md-12 col-lg-12 mb-3 mt-3">
+                    <ul >
+                    <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes" >Consulta 1/12/2021
+                            <span class="d-flex">
+
+                                <a href="/series/1/temporadas" class="btn btn-info btn-sm mr-1">
+                                    <i class="fas fa-external-link-alt"></i>
+                                </a>                          
+                            
+                            </span>     
+                        
+                        </li>
+                        <hr>
                     </ul>
                 </div>                
             </div>   
         </div>
     </div>
 
-    <div class="tab-pane fade" id="realizadas" role="tabpanel" aria-labelledby="realizadas-tab">
+    <div class="tab-pane fade" id="realizadas" role="tabpanel" aria-labelledby="realizadas-tab" >
         <div style="background-color: white;">
             <h3 class="ml-2 pt-3">Nome do Paciente</h3>
             <div class="row ml-4">
-                <div class="col col-sm-12 col-md-6 col-lg-6 mb-3 mt-3">
-                    <ul type="circle">
-                        <li>Consulta 1/12/12</li>
-                        <li>consulta 54565</li>
-                        <li>consulta 23</li>
+                <div class="col col-sm-12 col-md-12 col-lg-12 mb-3 mt-3">
+                    <ul >
+                        <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes" >Consulta 1/12/12
+                            <span class="d-flex">
+
+                                <a href="/series/1/temporadas" class="btn btn-info btn-sm mr-1">
+                                    <i class="fas fa-external-link-alt"></i>
+                                </a>
+                            
+                            </span>
+                            
+                        
+                        </li>
+                        <hr>
                     </ul>
                 </div>                
             </div>   
