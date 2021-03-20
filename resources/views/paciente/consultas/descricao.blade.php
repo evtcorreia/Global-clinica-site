9cb4d373fb81695ed4c7548aebadf90c3b76d14f
Lucas Lisboa <lucasalisboa.ll@gmail.com>
2021-03-20 11:41:25 -0300

    Up descricao
    Up descricao consulta

--- resources/views/paciente/consultas/descricao.blade.php
+++ resources/views/paciente/consultas/descricao.blade.php
@@ -6,35 +6,65 @@ Consulta
 
 @section('conteudo')
 
-<ul class="nav nav-tabs" id="consultaTab" role="tablist" >
+<div style="text-align:right; width:100%; padding:0;">
+    <input type="button" value="Voltar" onClick="history.go(-1)">
+</div>
+
+<ul class="nav nav-tabs" id="myTab" role="tablist" >
     <li class="nav-item col-sm-12 col-md-2 col-lg-2  mt-3" >
         <a class="nav-link active " id="consulta-tab" data-toggle="tab" href="#consulta" role="tab" aria-controls="consulta" aria-selected="true">Informações</a>
     </li>
     <li class="nav-item col-sm-12 col-md-2 col-lg-2 mt-3">
-        <a class="nav-link 3" id="receitas-tab" data-toggle="tab" href="#receitas" role="tab" aria-controls="receitas" aria-selected="false">Receitas</a>
+        <a class="nav-link 2" id="receitas-tab" data-toggle="tab" href="#receitas" role="tab" aria-controls="receitas" aria-selected="false">Receitas</a>
     </li>
     <li class="nav-item col-sm-12 col-md-2 col-lg-2 mt-3">
         <a class="nav-link 3" id="exames-tab" data-toggle="tab" href="#exames" role="tab" aria-controls="exames" aria-selected="false">Exames</a>
     </li>
 </ul>
 
-<input type="button" value="Voltar" onClick="history.go(-1)"> 
-
-<div class="tab-content" id="consultaTab">
+<div class="tab-content" id="myTabContent">
     <div class="tab-pane fade show active" id="consulta" role="tabpanel" aria-labelledby="consulta-tab">
         <div style="background-color: white;">
-            <h3 class="ml-2 pt-3">Dados Pessoais</h3>
-            <hr>
+            <h3 class="ml-2 pt-3">Consulta Data</h3>
             <div class="row ml-4">
                 <div class="col col-sm-12 col-md-6 col-lg-6 mb-3 mt-3">
                     <h6>Nome</h6>
+                    <p></p>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 mb-3 mt-3">
                     <h6>Sobrenome</h6>
+                    <p></p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
+    <div class="tab-pane fade" id="receitas" role="tabpanel" aria-labelledby="receitas-tab" >
+        <div style="background-color: white;">
+            <h3 class="ml-2 pt-3">Medicamentos</h3>
+            <div class="row ml-4">
+                <div class="col col-sm-12 col-md-12 col-lg-12 mb-3 mt-3">
+                    <ul >
+                        <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes"> Remedio X</li>
+                        <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes"> Remedio y</li>
+                    </ul>
+                </div>                
+            </div>   
+        </div>
+    </div>
+
+    <div class="tab-pane fade" id="exames" role="tabpanel" aria-labelledby="exames-tab">
+        <div style="background-color: white;">
+            <h3 class="ml-2 pt-3">Exames Realizados/Pedidos</h3>
+            <div class="row ml-4">
+                <div class="col col-sm-12 col-md-12 col-lg-12 mb-3 mt-3">
+                    <ul >
+                        <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes" >Exame X</li>
+                        <li class="list-group-item d-flex justify-content-between align-items-left lista-informacoes"> Exame Y</li>
+                    </ul>
+                </div>                
+            </div>   
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/paciente/index/index.blade.php
+++ resources/views/paciente/index/index.blade.php
@@ -1,6 +1,7 @@
 @extends ('layout')
 
 @section('cabecalho')
+
     Bem vindo {{$pessoa["pessoa_nome"]}} {{$pessoa["pessoa_sobrenome"]}}
 
 @endsection
