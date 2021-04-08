e2d997184fa8712cb8f3e5b4ec15db5cb1ff163e
Everton Correia <56456963+evtcorreia@users.noreply.github.com>
2021-04-08 19:34:26 -0300

    Mudança na ordem do menu Index
    MUdança na ordem dos menus na tela inicial do paciente

--- resources/views/paciente/index/index.blade.php
+++ resources/views/paciente/index/index.blade.php
@@ -9,17 +9,19 @@
 @section('conteudo')
 
     <div class="row row-cols-1 row-cols-md-3 g-4 ">   
-        <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
-            <a href="/paciente/informacao/{{$pessoa['pessoa_cpf']}}" >
+
+    <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
+            <a href="/paciente/agendamentos/index">
                 <div class="card card-menu-individual">
-                    <img src="../../img/usuario/usuario.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
+                    <img src="../../img/usuario/agendamento.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Dados Pessoais</h5>
+                        <h5 class="card-title">Agendamentos</h5>
                         <p class="card-text"></p>
                     </div>
                 </div>
             </a>
         </div>
+        
         
         <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
             <a href="/paciente/consultas/{{$pessoa['pessoa_cpf']}}" >
@@ -33,26 +35,29 @@
             </a>
         </div>
 
-        <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
-            <a href="/paciente/agendamentos/index">
-                <div class="card card-menu-individual">
-                    <img src="../../img/usuario/agendamento.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Agendamentos</h5>
-                        <p class="card-text"></p>
-                    </div>
-                </div>
-            </a>
-        </div>
+        
         <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
             <div class="card card-menu-individual">
-            <img src="../../img/usuario/contato.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
+                <img src="../../img/usuario/contato.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Recados</h5>
                 <p class="card-text"></p>
             </div>
             </div>
         </div>
+
+        <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
+            <a href="/paciente/informacao/{{$pessoa['pessoa_cpf']}}" >
+                <div class="card card-menu-individual">
+                    <img src="../../img/usuario/usuario.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Dados Pessoais</h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
     </div>
 
 @endsection
