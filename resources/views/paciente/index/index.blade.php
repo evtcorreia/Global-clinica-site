fcd6c126eb36c76b71e240085c6647373148e882
Lucas Lisboa <lucasalisboa.ll@gmail.com>
2021-06-05 15:43:37 -0300

    icones up

--- resources/views/paciente/index/index.blade.php
+++ resources/views/paciente/index/index.blade.php
@@ -13,7 +13,7 @@
     <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
             <a href="/agendamentos/index/{{$pessoa['prontuario_cod']}}">
                 <div class="card card-menu-individual">
-                    <img src="../../img/usuario/agendamento.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
+                    <img src="../../img/usuario/agendamento2.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Agendamentos</h5>
                         <p class="card-text"></p>
@@ -38,7 +38,7 @@
         
         <div class="col col-sm-12 col-md-12 col-lg-3 mb-3 mt-3">
             <div class="card card-menu-individual">
-                <img src="../../img/usuario/contato.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
+                <img src="../../img/usuario/contato3.png" class="card-img-top  mx-auto d-block mt-3 menu-paciente" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Recados</h5>
                 <p class="card-text"></p>
