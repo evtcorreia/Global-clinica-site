7d0998af9149b955cf1a630b9e4ff5c7c7ec0173
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2021-06-21 16:25:46 -0300

    tela de escolha

--- resources/views/entrar/selecao-tipo-acesso.blade.php
+++ resources/views/entrar/selecao-tipo-acesso.blade.php
@@ -17,13 +17,13 @@
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-        }
-        body {
+          }
+          body {
             font-family: 'Open Sans', sans-serif;
             background-image: url('../img/bg/globalBackground.jpg');
             background-color: #53A0E9;
-        }
-        .container-fluid {
+          }
+          .container-fluid {
             display: flex;
             justify-content:center;
             align-items:center;
@@ -32,81 +32,85 @@
             background-color: #ffffff;
             margin-top: 12%;
             padding-left: 15%;
-  
-            
-        }
+          }
 
-        .col{
-          width: 400px;    
-          
-          
-         
-        }
-        .icone{
-          color: #53A0E9;
-          margin: 0%;
-        }
+          .col{
+            width: 400px;    
+          }
 
-        .tipo{
-          
-        }
-        </style>
+          .icone{
+            color: #53A0E9;
+            margin: 0%;
+          }
 
-</head>
+    </style>
+  </head>
 
   <body>
 
-
-
-  <div class="container-fluid">
-    <div class="row">
-    @foreach($pessoas as $pessoa)  
-      @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 1)
-        <div class="col col-sm-12 col-md-12 col-lg-4 mb-3 mt-3">
-          <a href="/medico/index/{{$cpf}}">
-            <img src="../../img/usuario/medico.png" id="medico" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
-          </a>
-        </div>
-      @endif
-      
-      @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 2)
-        <div class="col col-sm-12 col-md-12 col-lg-4 mb-3 mt-3" tipo>      
-            <a href="/paciente/index/{{$cpf}}">
-              <img src="../../img/usuario/paciente.png" id="paciente" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
-            </a>
-        </div>
-      @endif
-
-      @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 3)
+         
+      <div class="row altura">
+       
+        @foreach($pessoas as $pessoa)
+          
+          @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 1)
+            @if(count($pessoas)==1)
+              <div class="col col-sm-12 col-md-12 col-lg-12">
+            @else
+                <div class="col col-sm-8 col-md-12 col-lg-3">
+                  @endif
+              <a href="/medico/index/{{$cpf}}">
+                <img src="../../img/usuario/medico.png" id="medico" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
+              </a>
+            </div>
+          @endif
         
-          <div class="col col-sm-12 col-md-12 col-lg-4 mb-3 mt-3  ">
-            <a href="/secretaria/index/{{$cpf}}">
-              <img src="../../img/usuario/recepcionista.png" id="secretaria" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
-            </a>
-          </div>
-      
-      @endif    
+          @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 2)
+          @if(count($pessoas)==1)
+              <div class="col col-sm-12 col-md-12 col-lg-12">
+            @else
+                <div class="col col-sm-8 col-md-12 col-lg-3">
+                  @endif      
+                <a href="/paciente/index/{{$cpf}}">
+                  <img src="../../img/usuario/paciente.png" id="paciente" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
+                </a>
+            </div>
+          @endif
 
-      @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 4)
-        
-          <div class="col col-sm-12 col-md-12 col-lg-4 mb-3 mt-3  ">
-            <a href="/adm/index/{{$cpf}}">
-              <img src="../../img/usuario/adm.png" id="adm" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
-            </a>
-          </div>
-      
-      @endif
+          @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 3)
+          @if(count($pessoas)==1)
+              <div class="col col-sm-12 col-md-12 col-lg-12">
+            @else
+                <div class="col col-sm-8 col-md-12 col-lg-3">
+                  @endif
+              <a href="/secretaria/index/{{$cpf}}">
+                <img src="../../img/usuario/recepcionista.png" id="secretaria" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
+              </a>
+            </div>
+          @endif    
 
-    @endforeach
-      
+          @if ($pessoa['tipo_pessoa_tpessoa_cod'] == 4)
+          @if(count($pessoas)==1)
+              <div class="col col-sm-12 col-md-12 col-lg-12">
+            @else
+                <div class="col col-sm-8 col-md-12 col-lg-3">
+                  @endif
+              <a href="/adm/index/{{$cpf}}">
+                <img src="../../img/usuario/adm.png" id="adm" class="card-img-top  mx-auto d-block mt-3 menu-paciente-inicio" alt="...">
+              </a>
+            </div>
+          @endif
 
-    </div>
-  </div>
+        @endforeach
+       
+         
+      </div>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
   </body>
 </html>
